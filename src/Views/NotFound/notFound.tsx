d898e10cf777f36c587ba5e569dcc25afd03834a
khaled-codeur <manou-codeur@outlook.com>
2022-03-28 16:01:28 +0200

    done with <NotFound />

--- src/Views/NotFound/notFound.tsx
+++ src/Views/NotFound/notFound.tsx
@@ -1,5 +1,13 @@
+import { ReactComponent as NotFoundVector } from "../../Assets/img/notFound-Vector.svg";
+
+import "./notFound.scss";
+
 const NotFound = () => {
-  return <h1>NotFound</h1>;
+  return (
+    <div className="notFound">
+      <NotFoundVector className="notFound__svg" />
+    </div>
+  );
 };
 
 export default NotFound;
